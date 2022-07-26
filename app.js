22b10633f2d9ef982561b341f0516e24bac9c2ef
Yu-Ting Wu <a22627046@gmail.com>
2022-07-27 03:06:58 +0800

    feat: add new commands to the line-bot

--- app.js
+++ app.js
@@ -16,12 +16,60 @@ const linebotParser = bot.parser()
 
 
 bot.on('message', function (event) {
-  event.reply(event.message.text)
-    .then(function (data) {
-      // success
-    }).catch(function (error) {
-      // error
-    })
+  console.log(event)
+  switch (event.message.text.toLowerCase().replace(/\s*/g, "")) {
+    case 'wifi密碼':
+      event.reply({
+        type: 'text',
+        text: '密碼為 XXXXXXXXX'
+      })
+      break
+    case '黃沐妍':
+      event.reply([{
+        type: 'text',
+        text: '這是黃沐妍(小豬)'
+      }, {
+        type: 'uri',
+        uri: 'https://zh.m.wikipedia.org/zh-tw/%E9%BB%83%E6%B2%90%E5%A6%8D'
+      }])
+      break
+    case '熊熊':
+      event.reply([
+        {
+          type: 'text',
+          text: '這是熊熊'
+        }, {
+          type: 'uri',
+          uri: 'https://zh.wikipedia.org/zh-tw/%E5%8D%93%E6%AF%93%E5%BD%A4'
+        }
+      ])
+      break
+    case '周曉涵':
+      event.reply([{
+        type: 'text',
+        text: '這是周曉涵'
+      }, {
+        type: 'uri',
+        uri: 'https://moviecool.asia/person/qd_91hzgS'
+      }])
+      break
+    case '邵雨薇':
+      event.reply([
+        {
+          type: 'text',
+          text: '這是邵雨薇'
+        }, {
+          type: 'uri',
+          uri: 'https://www.elle.com/tw/columnist/g36614244/ivy-shao-special-column-3/'
+        }
+      ])
+      break
+    default:
+      event.reply({
+        type: 'text',
+        text: '指令錯誤，請再試一次'
+      })
+  }
 })
 
 app.post('/', linebotParser)
